8c0f4f0ba0d949c363c0fa23e43a5263272e78a3
juniorespriella99 <juniorespriella99@gmail.com>
2024-10-01 17:17:20 -0500

    fix: creacion de componente login y configuracion de ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path:'**',
+        redirectTo: '/login'
+    }
+];
+
+@NgModule(
+    {
+        imports: [RouterModule.forRoot(routes)],
+        exports:[RouterModule]
+    }
+)
+export class AppRoutingModule {}
