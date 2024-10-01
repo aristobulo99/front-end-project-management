10825e0bcc1733a3748032b3da116a73356d2a84
juniorespriella99 <juniorespriella99@gmail.com>
2024-10-01 17:29:10 -0500

    fix: instalacion de tailwind y implementacion del gradiente

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        pinkCustom: '#FF70DB',
+        blueCustom: '#0037FF',
+      },
+    },
+  },
+  plugins: [],
+}
+
