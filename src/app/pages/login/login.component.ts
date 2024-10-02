5fdfd1c3a2a7819e0f66a9ccb96430f00cef83a5
juniorespriella99 <juniorespriella99@gmail.com>
2024-10-02 13:26:49 -0500

    fix: creacion del componente button

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardLComponent } from '../../shared/components/atom/card-l/card-l.component';
 import { InputComponent } from '../../shared/components/atom/input/input.component';
+import { ButtonComponent } from '../../shared/components/atom/button/button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CardLComponent, InputComponent],
+  imports: [CardLComponent, InputComponent, ButtonComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
