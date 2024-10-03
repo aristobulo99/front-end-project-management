07245debcd5b0567a3cf6d19781da8f8f93d5ae9
juniorespriella99 <juniorespriella99@gmail.com>
2024-10-03 15:05:07 -0500

    fix: implementacion de ReactiveForm y estilo de boton bloqueado

--- src/app/shared/components/organisms/login-form/login-form.component.ts
+++ src/app/shared/components/organisms/login-form/login-form.component.ts
@@ -1,14 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InputComponent } from '../../atom/input/input.component';
 import { ButtonComponent } from '../../atom/button/button.component';
+import { InputControl } from '../../../../core/interfaces/input.interface';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControlPipe } from '../../../pipe/form-control/form-control.pipe';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [InputComponent, ButtonComponent],
+  imports: [
+    InputComponent, 
+    ButtonComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    FormControlPipe
+  ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
+
+  public formsInputs: InputControl[] = [
+    {
+      type: 'email',
+      icon: 'mail',
+      placeholder: 'Escriba el correo electronico',
+      formInfo: {
+        formName: 'email',
+        validatorRequered: true,
+        validatorEmail: false,
+      }
+    },
+    {
+      type: 'password',
+      icon: 'lock',
+      placeholder: 'Escriba la contraseña',
+      formInfo: {
+        formName: 'password',
+        validatorRequered: true
+      }
+    }
+  ];
+  public formLogin: FormGroup = new FormGroup({});
+
+  constructor(
+    private fb: FormBuilder
+  ){}
+
+  ngOnInit(): void {
+    this.initFormLogin();
+  }
+
+  initFormLogin(){
+    this.formLogin = this.fb.group(
+      {
+        email: new FormControl<string>('',[Validators.required, Validators.email]),
+        password: new FormControl<string>('', [Validators.required])
+      }
+    )
+  }
+
+  fieldValidation(field: string): boolean {
+    return (this.formLogin.get(field)?.valid || !this.formLogin.get(field)?.touched) as boolean;
+  }
+
+  fieldHasError(field: string, errorCode: string): boolean {
+    return (this.formLogin.get(field)?.touched &&  this.formLogin.get(field)?.hasError(errorCode)) as boolean
+  }
 
 }
