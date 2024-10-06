171e09a07af17ae77c733b9e324714fceaf500c0
juniorespriella99 <juniorespriella99@gmail.com>
2024-10-06 15:22:40 -0500

    fix: ajustes de card para la creacion de proyectos

--- src/app/shared/components/molecules/card-project/card-project.component.ts
+++ src/app/shared/components/molecules/card-project/card-project.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { IconComponent } from '../../atom/icon/icon.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-card-project',
   standalone: true,
   imports: [
+    NgClass,
     IconComponent
   ],
   templateUrl: './card-project.component.html',
@@ -13,5 +15,13 @@ import { IconComponent } from '../../atom/icon/icon.component';
 export class CardProjectComponent {
 
   @Input() featured: boolean = false;
+  @Input() fieldCreation: boolean = false;
+
+  get styleSection(){
+    return {
+      'justify-between border-[0.063rem] ': !this.fieldCreation,
+      'justify-center items-center border-dashed border-2 ': this.fieldCreation
+    }
+  }
 
 }
